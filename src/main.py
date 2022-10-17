dd4564c972a4573764118e197014c2bd26ca5e0e
Peter Pham <ptrpham2468@gmail.com>
2022-10-17 07:14:26 -0500

    created the initial window

--- src/main.py
+++ src/main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from kivy.app import App
+from kivy.uix.widget import Widget
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class MainWidget(Widget):
+    pass
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+class SchedulerApp(App):
+    pass
 
+if __name__ == "__main__":
+    SchedulerApp().run()
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
